dabc459ee9ac85a9c94efbe6282d5254ba7fe1e6
Chhai Chivon <chivon.chhai.kh@gmail.com>
2023-03-22 22:27:21 +0700

    Initail VueJS Demo

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<body>
+ <h2>About</h2>
+</body>
+<template>
+     <div v-html="htmlString"></div>
+
+  </template>
+  
+  <script>
+    export default {
+        name: 'AboutMe',
+    }
+  </script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Home Page</h1>
+</template>
+<script>
+/* ✓ GOOD */
+export default {
+  name: 'Home-Page',
+  // ...
+}
+</script>
